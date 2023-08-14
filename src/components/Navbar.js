d56f2e779ae29d2df030c3c2338c91d21b7ef4ee
Naman <namanb454@gmail.com>
2023-08-14 10:35:41 +0530

    new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,9 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Suwone</Link>
+        <Link href="/">
+          <img className='logo1' src='animation.gif' />
+        </Link>
       </p>
 
 
